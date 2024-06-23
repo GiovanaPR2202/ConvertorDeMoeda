68c0566a0a87d9bd971ae6d64db0b9fe9a812340
GiovanaPR2202 <giovanapereira140@gmail.com>
2024-06-23 04:39:57 -0300

    Atualizacao final

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 
 ## Convertor de Moeda 
+
+![ConvertorDeMoedaResultado.png](src%2Fmain%2Fresources%2Fassets%2FConvertorDeMoedaResultado.png)
+## Descrição
+
+Neste emocionante desafio de programação, convidamos você a construir o seu próprio Conversor de Moedas. Você aprenderá a fazer requisições a uma API de taxas de câmbio, a manipular dados JSON e, por fim, a filtrar e exibir as moedas de interesse. Prepare-se para uma experiência prática e empolgante no desenvolvimento Java!
+
+## Objetivo
+
+Desenvolver um Conversor de Moedas que ofereça interação textual (via console) com os usuários. O programa deve proporcionar no mínimo 6 opções distintas de conversões de moedas em um menu. As taxas de conversão serão obtidas dinamicamente através de uma API, garantindo dados precisos e em tempo real para uma experiência mais atualizada e eficaz.
+
+## Tecnologias Utilizadas
+
+- **Java**: Linguagem de programação principal.
+- **ExchangeRate-API**: API utilizada para obter taxas de câmbio em tempo real.
+- **Gson**: Biblioteca da Google para manipulação de JSON.
+
+## Funcionalidades
+
+- **Interação via console**: Os usuários podem selecionar as moedas para conversão através de um menu textual.
+- **Atualização em tempo real**: As taxas de câmbio são obtidas de uma API, garantindo valores atualizados.
+- **Múltiplas opções de conversão**: Suporte para pelo menos 6 tipos diferentes de conversões de moeda.
+
+## Pré-requisitos
+
+- **Java** (JDK 8 ou superior)
+- **Gson** (para manipulação de JSON)
+- **Internet** (para acessar a API de taxas de câmbio)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,82 @@
 package org.example;
 
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public Main() throws IOException {
     }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        String enderecourl = "https://v6.exchangerate-api.com/v6/54a7f2507ca2dd3efe90f930/latest/USD";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(enderecourl))
+                .build();
+
+        JsonObject jsonObject;
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        jsonObject = new Gson().fromJson(response.body(), JsonObject.class);
+
+        List<Map<String, Object>> convertores = new ArrayList<>();
+
+
+        try{
+            System.out.println("Digite o valor da moeda escolhida:");
+            double valor = Double.parseDouble(scanner.nextLine());
+
+            System.out.println("Digite o simbolo da moeda escolhida(EX:eal (BRL); Dólar Americano (USD),Euro (EUR); e etc ... ");
+            var simbolo1 = scanner.nextLine();
+
+            double moeda1 = jsonObject.getAsJsonObject("conversion_rates").get(simbolo1).getAsDouble();
+
+            System.out.println("Digite o segundo simbolo da moeda escolhida(EX:eal (BRL); Dólar Americano (USD),Euro (EUR); e etc ... ");
+            var simbolo2 = scanner.nextLine();
+            double moeda2 = jsonObject.getAsJsonObject("conversion_rates").get(simbolo2).getAsDouble();
+
+
+            converter(valor,simbolo1,simbolo2,moeda1,moeda2);
+
+            Map<String, Object> convertore = new HashMap<>();
+            convertore.put("Moeda_Primairia: ",simbolo1);
+            convertore.put("Primeiro-valor: ",valor);
+            convertore.put("Moeda-Secundaria: ",simbolo2);
+            convertore.put("Valor-Convertido: ", Math.round((valor * moeda2 / moeda1) * 100.0) / 100.0);
+
+            convertores.add(convertore);
+
+
+        }catch(Exception e){
+            System.out.println("Ocorreu um erro ao inserir valores!");
+        }
+
+
+    }
+
+
+    public static void converter(double valor, String simbolo1, String simbolo2, double moeda1, double moeda2) {
+        System.out.println(valor + " em " + simbolo1 + " equivale a " + (valor * (moeda2 / moeda1)) + " em " + simbolo2);
+    }
+
+
 }
